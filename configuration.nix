8d19a74b93a3609d3ae1cb9fc7a468ee60fc855c
Yevhen Shymotyuk <fly1ngdream@pm.me>
2020-05-31 22:55:49 +0300

    Add switch-on-connect pulseaudio module

--- configuration.nix
+++ configuration.nix
@@ -68,6 +68,9 @@ hardware = {
     support32Bit = true;
     extraModules = [ pkgs.pulseaudio-modules-bt ];
     package = pkgs.pulseaudioFull;
+    extraConfig = ''
+      load-module module-switch-on-connect
+    '';
   };
 };
 
