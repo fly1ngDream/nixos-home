7aa48266c40dee92c9f2beead1507b384ce5d5b8
Yevhen Shymotyuk <fly1ngdream@pm.me>
2020-07-22 00:19:10 +0300

    Add autoUpgrade.channel

--- configuration.nix
+++ configuration.nix
@@ -133,10 +133,8 @@
 
   time.timeZone = "Europe/Kiev";
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "20.03"; # Did you read the comment?
-
+  system = {
+    stateVersion = "20.03";
+    autoUpgrade.channel = "https://nixos.org/channels/nixos-unstable";
+  };
 }
